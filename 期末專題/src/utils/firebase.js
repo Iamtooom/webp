2a78e023e297d068dc8fb9a26e1444425c83be08
Iamtooom <100269288+Iamtooom@users.noreply.github.com>
2022-06-22 21:24:19 +0800

    Add files via upload

---
+++ 期末專題/src/utils/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDT2sRSEWGStKb-V1tC6LbwOEc7zhiizOI",
+  authDomain: "b0929020.firebaseapp.com",
+  projectId: "b0929020",
+  storageBucket: "b0929020.appspot.com",
+  messagingSenderId: "102496694365",
+  appId: "1:102496694365:web:f0de82e57c78b067e4ddb4"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
